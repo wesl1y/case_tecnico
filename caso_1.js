09cbd4428805edd6d89128928fc9216dc6cfecfa
wesl1y <weslly1234vitor@gmail.com>
2024-12-12 19:55:11 -0300

    :rocket: making functions

--- caso_1.js
+++ caso_1.js
@@ -1,16 +1,52 @@
-const fs = require('fs');
+const dataBase1 = "./broken_database_1.json";
+const dataBase2 = "./broken_database_2.json";
+
+function ajustingData(data)
+{
+  return data.map(item => {
+    if(item.marca)
+    {
+      item.marca = item.marca.replace(/æ/g, 'a').replace(/ø/g, 'o');
+    }
+    if(item.nome){
+      item.nome = item.nome.replace(/æ/g, 'a').replace(/ø/g, 'o');
+    }
+
+    if(typeof item.vendas === "string")
+    {
+      item.vendas = parseInt(item.vendas);
+    }
+
+    return item
+  });
+}
+
+
+function readJson(path)
+{
+  return fetch(path)
+    .then(res => {
+      if(!res){
+        throw new Error("Json not accepted! Please try again");
+      }
+      return res.json()
+    })
+
+    .then(data => {
+      
+      const correctData = ajustingData(data);
+
+      console.log(correctData);
+    })
+}
 
 
 
-fs.readFile('./broken_database_1.json', (err, data) => {
-  if (err) {
-    console.error('Erro ao ler o arquivo:', err);
-    return;
-  }
-  try {
-    const jsonData = JSON.parse(data); // Converte string para objeto JSON
-    console.log(jsonData);
-  } catch (parseError) {
-    console.error('Erro ao analisar o JSON:', parseError);
-  }
-});
+
+Promise.all([
+  readJson(dataBase1),
+  readJson(dataBase2),
+])
+  .then(() => {
+    console.log('Json files fixed');
+  });
